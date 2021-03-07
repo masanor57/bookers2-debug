1873cce180ac00cd9affa8791111ea5b9c5df4f7
EC2 Default User <ec2-user@ip-172-31-88-98.ec2.internal>
2021-03-07 11:20:27 +0000

    応用課題４

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/about' => 'homes#about'
   
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]do
+   resource :relationships, only: [:create, :destroy]
+  get 'followings' => 'relationships#followings'
+  get 'followers' => 'relationships#followers'
+  end
   resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
