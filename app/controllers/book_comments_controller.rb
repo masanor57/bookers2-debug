9a119fa161261297b14adad6985cf18c63566fb9
EC2 Default User <ec2-user@ip-172-31-88-98.ec2.internal>
2021-05-29 10:44:24 +0000

    ajax1

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -6,7 +6,7 @@ class BookCommentsController < ApplicationController
     @book_comment.book_id = @book.id
     @book_comment.user_id = current_user.id
 	if @book_comment.save
-  	redirect_to book_path(@book.id)
+  
 	else
 	  render 'books/show'
 	end
@@ -16,7 +16,7 @@ class BookCommentsController < ApplicationController
     @book = Book.find(params[:book_id])
   	book_comment = @book.book_comments.find(params[:id])
     book_comment.destroy
-	  redirect_to request.referer
+	 
   end
   
   private
